e9cb0575aff92cd103ce4e7dca0c68d2958156d2
cauesued <cauesued@gmail.com>
2026-01-06 18:43:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 .card-produto img {
-    width: 200px;                    /* largura da imagem do produto */
-    border-radius: 10px;             /* cantos arredondados na imagem */
+    width: 200px;                    
+    border-radius: 10px;             
     margin-bottom: 10px;
     }
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s")
