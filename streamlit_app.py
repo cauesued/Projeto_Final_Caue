632888771012c534f09a8c661e9a48393bad016f
cauesued <cauesued@gmail.com>
2026-01-06 18:56:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s', width=600)
 
-st.title("Projeto Fin# 1. Inicialize st.session_state para armazenar o valor
+st.title("Projeto Final: Calculadora")
+# 1. Inicialize st.session_state para armazenar o valor
 if 'numero_atual' not in st.session_state:
     st.session_state['numero_atual'] = 0
 
