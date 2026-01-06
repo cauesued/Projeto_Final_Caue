d5e81e71a7cc2f2b5940c4827d6e66213f5cc66c
cauesued <cauesued@gmail.com>
2026-01-06 18:12:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Meu Projeto Final: Calculadora")
-st.write("Calculadora")
+st.img(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s)
+  
