5302e8b8cfdb2411e43c2b820712b28f48ca4c03
cauesued <cauesued@gmail.com>
2026-01-07 15:02:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s', width=600)
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s', width=600) # A imagem
 
-st.divider()
+st.divider() # Para criar as linhas
 
-st.title("Projeto Final: Calculadora")
+st.title("Projeto Final: Calculadora") # O titulo 
 
 st.set_page_config(layout="wide")
 
