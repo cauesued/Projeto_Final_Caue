b1ce89fb57a5b9a6e65264c2881719d1ebf28a57
cauesued <cauesued@gmail.com>
2026-01-07 14:58:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-puJcpOvnq50F4JR1O-g_mscVBpo25OmFEA&s', width=600)
 
+st.title("Acima da linha")
+
+st.divider()
+
+st.title("Abaixo da linha")
+
 st.title("Projeto Final: Calculadora")
 
 st.set_page_config(layout="wide")
